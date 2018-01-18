4106af1addca1e2fd31908269d5a57b9e3c0940f
fearless-hasan <hasan.bubt.40@gmail.com>
2018-01-18 12:56:34 +0600

    day-26+27

---
+++ laravel-first/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Web App Develop-PHP
+ * Date: 1/16/2018
+ * Time: 11:07 AM
+ */
+?>
+
+<a href="<?php echo route('/'); ?>">Home</a>
+<a href="<?php echo route('/about'); ?>">About</a>
+<h1>This is home Page</h1>
